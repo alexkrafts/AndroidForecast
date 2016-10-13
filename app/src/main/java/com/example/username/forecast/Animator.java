e4b5b144b1fe31e39736462030fceef205c00c11
Alexey Kravtsov <ka@70.ru>
2016-10-13 17:53:40 +0300

    fullscreen|gps fix

---
+++ app/src/main/java/com/example/username/forecast/Animator.java
@@ -0,0 +1,89 @@
+package com.example.username.forecast;
+
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.username.forecast.R;
+
+/**
+ * Created by username on 10/12/2016.
+ */
+public class AnimatorClass {
+    private static final int RightToLeft = 1;
+    private static final int LeftToRight = 2;
+    private static final int DURATION = 500;
+
+    private ValueAnimator mCurrentAnimator;
+    private final Matrix mMatrix = new Matrix();
+    private ImageView mImageView;
+    private float mScaleFactor;
+    private int mDirection = RightToLeft;
+    private RectF mDisplayRect = new RectF();
+
+    public AnimatorClass(View imageView){
+        mImageView = (ImageView) imageView;
+    }
+    private void startMoving(final Drawable drawable) {
+
+        mImageView.post(new Runnable() {
+            @Override
+            public void run() {
+                mScaleFactor = (float) mImageView.getHeight() / (float) drawable.getIntrinsicHeight();
+                mMatrix.postScale(mScaleFactor, mScaleFactor);
+                mImageView.setImageMatrix(mMatrix);
+                animate();
+            }
+        });
+    }
+
+    private void animate() {
+        updateDisplayRect();
+        if (mDirection == RightToLeft) {
+            animate(mDisplayRect.left, mDisplayRect.left - (mDisplayRect.right - mImageView.getWidth()));
+        } else {
+            animate(mDisplayRect.left, 0.0f);
+        }
+    }
+
+    private void animate(float from, float to) {
+        mCurrentAnimator = ValueAnimator.ofFloat(from, to);
+        mCurrentAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (Float) animation.getAnimatedValue();
+
+                mMatrix.reset();
+                mMatrix.postScale(mScaleFactor, mScaleFactor);
+                mMatrix.postTranslate(value, 0);
+
+                mImageView.setImageMatrix(mMatrix);
+
+            }
+        });
+        mCurrentAnimator.setDuration(DURATION);
+        mCurrentAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(android.animation.Animator animation) {
+                if (mDirection == RightToLeft)
+                    mDirection = LeftToRight;
+                else
+                    mDirection = RightToLeft;
+
+                animate();
+            }
+        });
+        mCurrentAnimator.setDuration(35000);
+        mCurrentAnimator.start();
+    }
+
+    private void updateDisplayRect() {
+        mDisplayRect.set(0, 0, mImageView.getDrawable().getIntrinsicWidth(), mImageView.getDrawable().getIntrinsicHeight());
+        mMatrix.mapRect(mDisplayRect);
+    }
+
+}
